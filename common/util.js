8001745d6a4d422d9865da556642edde683fc9f6
yjlee0235 <yjlee0235@naver.com>
2023-09-06 17:58:22 +0900

    Feat: response 및 시간 포멧팅 메서드 추가

---
+++ common/util.js
@@ -0,0 +1,12 @@
+module.exports.response = (statusCode, body) => {
+    console.log("on response. statusCode: %s, body: %j", statusCode, body);
+    return {
+        statusCode: statusCode || 200,
+        body: JSON.stringify(body || {}, null, 2),
+    };
+}
+
+module.exports.formatDate = (date) =>
+    date.toISOString()
+        .replace(/T/, ' ')
+        .replace(/\..+/, '');
